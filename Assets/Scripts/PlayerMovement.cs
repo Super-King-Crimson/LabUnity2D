079a67227c87511f93a8952759f4bb8709972669
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-08-09 02:54:46 -0400

    testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int hi = 10;
     }
 
     // Update is called once per frame
